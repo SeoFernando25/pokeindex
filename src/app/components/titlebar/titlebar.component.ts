138f52a76599582167af9b443261221a8cc82941
Fernando Nogueira <abert036@uottawa.ca>
2022-09-22 20:46:10 -0400

    add: hidding scrollbar

--- src/app/components/titlebar/titlebar.component.ts
+++ src/app/components/titlebar/titlebar.component.ts
@@ -1,7 +1,8 @@
 import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
-import { Subscription } from 'rxjs';
+import { last, Subscription } from 'rxjs';
+import { PokeSearchService } from 'src/app/services/poke-search.service';
 import { PokedexService } from 'src/app/services/pokedex.service';
 
 @Component({
@@ -10,69 +11,102 @@ import { PokedexService } from 'src/app/services/pokedex.service';
   styleUrls: ['./titlebar.component.scss']
 })
 export class TitlebarComponent implements OnInit, OnDestroy {
+
   @ViewChild('searchInput') searchRef: ElementRef | null = null;
   public lastStrSize = 0;
-  public searchValue: string = "";
 
+  searchBarValue: string = "";
   routerSubscription: Subscription | undefined;
 
 
-  constructor(public pokedex: PokedexService, public router: Router, public translate: TranslateService) {
+  constructor(public pokeSearch: PokeSearchService, public pokedex: PokedexService, public router: Router, public translate: TranslateService) {
+    this.searchBarValue = localStorage.getItem("search") || this.pokeSearch.searchValue.getValue();
   }
 
+
+  searchValueSubscription: Subscription | undefined;
   ngOnInit(): void {
-    // Check if there is a hash in the url
-    this.searchValue = localStorage.getItem("search") || "";
+    // this.searchValue = this.pokedex.identifierToReadableName(this.searchValue);
     // On press "/" focus on search bar
-    document.addEventListener("keydown", (event) => {
-      if (event.key === "/" && this.searchRef) {
-        // window.scrollTo(0, 0); (This does not work on webKit)
-        this.searchRef.nativeElement.scrollTop = 100;
-        this.searchRef.nativeElement.focus();
-        this.searchValue = "";
-
-        // Remove the last character from the search bar
-        // HACK: For some reason, even after returning false, the mat-input still consumes the "/" character
-        setTimeout(() => {
-          if (this.searchRef) {
-            this.searchRef.nativeElement.value = this.searchRef.nativeElement.value.slice(0, -1);
-            this.searchRef.nativeElement.select();
-          }
-        }, 0);
+    document.addEventListener("keydown", this.searchBarKeyListenerBinded);
+    document.addEventListener("scroll", this.scrollListenerBinded);
+    this.searchValueSubscription = this.pokeSearch.searchValue.subscribe((results) => {
+      // If searchBarValue is different from searchValue, update it
+      if (this.searchBarValue !== results) {
+        this.searchBarValue = results;
       }
     });
   }
 
-  ngAfterViewInit(): void {
-    this.routerSubscription = this.router.events.subscribe((event) => {
-      if (event instanceof NavigationEnd) {
-        // Reload the search bar text in case any changes were made
-        this.searchValue = localStorage.getItem("search") || this.pokedex.previousSearch;
-        this.searchValue = this.pokedex.identifierToReadableName(this.searchValue);
-      }
-    });
+  searchBarKeyListenerBinded = this.searchBarKeyListener.bind(this);
+  searchBarKeyListener(event: any) {
+    if (event.key === "/" && this.searchRef) {
+      // window.scrollTo(0, 0); (This does not work on webKit)
+      this.searchRef.nativeElement.scrollTop = 100;
+      this.searchRef.nativeElement.focus();
+    }
   }
+
+  // Scroll variables
+  lastScroll = 0;
+  showNavBar = true;
+  scrollListenerBinded = this.scrollListener.bind(this);
+  scrollListener(event: any) {
+    let scrollDiff = this.lastScroll - window.scrollY;
+    this.lastScroll = window.scrollY;
+
+    if (window.scrollY === 0 || scrollDiff > 0) {
+      this.showNavBar = true;
+    } else {
+      this.showNavBar = false;
+    }
+  }
+
+
 
   ngOnDestroy(): void {
+    document.removeEventListener("keydown", this.searchBarKeyListenerBinded);
+    document.removeEventListener("scroll", this.scrollListenerBinded);
+    if (this.searchValueSubscription) {
+      this.searchValueSubscription.unsubscribe();
+    }
     if (this.routerSubscription) {
       this.routerSubscription.unsubscribe();
     }
   }
 
+
+
+  onKeyPress(event: KeyboardEvent) {
+    let inp = event.key;
+
+    // Only allow alphanumeric characters and spaces
+    if (/[a-zA-Z0-9 \"]/.test(inp)) {
+      return true;
+    } else {
+      event.preventDefault();
+      return false;
+    }
+  }
+
   public onFocus() {
-    // If not on /search, redirect to /search
-    if (this.router.url !== "/search") {
+    // Scroll to top
+    window.scrollTo(0, 0);
+    window.scrollY = 0;
+    if (!this.router.url.startsWith("/search")) {
+      console.log("Redirecting to search"); // And add location hash
       this.router.navigate(["/search"]);
     }
   }
 
   public onInput(event: any) {
-    this.pokedex.nameFilterPokemon(event.target.value as string);
+    this.onFocus();
+    this.pokeSearch.searchValue.next(event.target.value as string);
   }
 
   public onSubmit() {
     console.log("Submitted");
-    let pokemonsResult = this.pokedex.filteredPokemon.getValue();
+    let pokemonsResult = this.pokeSearch.searchResults.getValue();
     // If there are no results, do nothing
     if (pokemonsResult.length === 0) {
       return;
